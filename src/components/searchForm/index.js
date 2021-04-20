a20c55f5fd48e0ef8e791afc841f0c313893c184
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-20 17:11:34 +0100

    move username state to parent

--- src/components/searchForm/index.js
+++ src/components/searchForm/index.js
@@ -1,8 +1,7 @@
 import React, {useState} from "react";
 import './style.css'
 
-const SearchForm = () => {
-    const [ username, setUsername ] = useState("");
+const SearchForm = ({setUsername}) => {
     const [ nameInput, setNameInput ] = useState("");
 
     const handleInput = e => setNameInput(e.target.value);
@@ -14,8 +13,6 @@ const SearchForm = () => {
     };
      return (
         <>
-        <h3 aria-label="Form" id="Form"> {username ? username : 'Friend'}, Your repositories are below:</h3>
-
         <form onSubmit={handleFormSubmit}>
           <label htmlFor="username">GitHub Username: </label>
           <input type="text" id="username" name="username" placeholder="Please enter your username" value={nameInput} onChange={handleInput}/>
