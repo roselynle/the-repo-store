11a41a80336a0e655a8b8f595aef071028f382a0
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-20 16:56:13 +0100

    add username state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, {useState} from 'react'
-import {SearchForm, RepoList} from './components';
+import React, { useState } from 'react'
+import { SearchForm, RepoList } from './components';
 
 import './App.css'
 
 function App() {
 
-    const [username, setUsername] = useState("theduckfliesagain");
+  const [username, setUsername] = useState("theduckfliesagain");
 
-    return (
-      <>
-       <h1>Hello World!</h1>
+  return (
+    <>
+      <h1>Hello World!</h1>
 
-       < RepoList username={username}/>
-      </>
-    )
+      < RepoList username={username} />
+    </>
+  )
 }
 
 export default App
