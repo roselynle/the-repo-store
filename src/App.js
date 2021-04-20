697e489922c07e7f0c7d3bb0c4a305db974df05c
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-20 16:30:58 +0100

    add repo list to main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {SearchForm, RepoList} from './components';
 
 import './App.css'
 
 function App() {
+
+    const [username, setUsername] = useState("theduckfliesagain");
+
     return (
       <>
        <h1>Hello World!</h1>
+
+       < RepoList username={username}/>
       </>
     )
 }
