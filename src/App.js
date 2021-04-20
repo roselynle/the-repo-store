4029af9865b3841999c88e67465f45ad599f2df5
Roselyn Le <roselynle95@gmail.com>
2021-04-20 23:15:41 +0100

    make separate component for header to keep App.js tidy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import { SearchForm, RepoList } from "./components";
+import { SearchForm, RepoList, Header} from "./components";
 import "./App.css";
 
 function App() {
-    const [username, setUsername] = useState("roselynle");
+    const [username, setUsername] = useState("");
 
     return (
         <>
-                <h1 id="main-heading">Your Repo Story</h1>
-                <SearchForm setUsername={setUsername} />
-                <div id="text-container">
-                    <h3 aria-label="Form" id="Form">
-                        {" "}
-                        {username ? username : "Coder"}, your repositories
-                        are below:
-                    </h3>
-                    <RepoList username={username} />
-                </div>
+            <Header />
+            <SearchForm setUsername={setUsername} />
+            <div id="text-container">
+                <h3 aria-label="Form" id="Form">
+                    {" "}
+                    {username ? username : "Coder"}, your repositories are
+                    below:
+                </h3>
+                <RepoList username={username} />
+            </div>
         </>
     );
 }
