24b82f1b06495c225ac439aaaf1cf092496d0000
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-20 16:56:22 +0100

    update exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export {default as UserCard} from './UserCard'
 export {default as RepoCard} from './RepoCard'
 export {default as RepoList} from './RepoList'
 export {default as SearchForm} from './SearchForm'
