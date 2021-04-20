93285c4aeec3db34af9ecfe679671ad49f1dc9fd
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-20 15:27:09 +0100

    add basic repo card structure

---
+++ src/components/repoCards/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BiGitRepoForked, BiStar,  } from "react-icons/bi";
+import { BsEyeFill } from "react-icons/bs";
+
+const repoCard = ({ repo }) => {
+    return (
+        <div className="repo">
+            <div className="repo-info">
+                <a href={repo.url} target="_blank">
+                    <h2>{repo.title}</h2>
+                </a>
+                <p>{repo.description}</p>
+            </div>
+            <div className="repo-details">
+                <div className="forks">
+                    <BiGitRepoForked /><span>{ repo.forks_count}</span>
+                </div>
+                <div className="stars">
+                    <BiStar /><span>{ repo.stargazers_count}</span>
+                </div>
+                <div className="watchers">
+                    <BsEyeFill /><span>{ repo.watchers_count}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default repoCard;
