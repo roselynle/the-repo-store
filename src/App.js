9a3ae22d361910ce04031089448c92e5e438cdd7
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-04-20 16:54:39 +0100

    add search Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import {SearchForm} from './components'
 import './App.css'
 
 function App() {
     return (
       <>
+      
+     
        <h1>Hello World!</h1>
+       <SearchForm />
+       
       </>
     )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export {default as repoCards} from './repoCards'
 export {default as repoData} from './repoData'
-export {default as searchForm} from './searchForm'
+export {default as SearchForm} from './SearchForm'
